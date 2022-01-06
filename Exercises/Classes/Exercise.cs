5133fde2cf1c338b55d14440632436fbd6240eca
amyjprogrammer <amyjprogrammer@gmail.com>
2022-01-06 16:45:17 -0500

    post exercise

--- Exercises/Classes/Exercise.cs
+++ Exercises/Classes/Exercise.cs
@@ -9,17 +9,28 @@ namespace Exercises.Classes
     {
         public static void Start() 
         {
-            var first = new Stopwatch();
-            first.Start();
-            Thread.Sleep(100);
-            first.Stop();
-            Console.WriteLine($"First stopwatch is {first.Duration}");
+            /* var first = new Stopwatch();
+             first.Start();
+             Thread.Sleep(100);
+             first.Stop();
+             Console.WriteLine($"First stopwatch is {first.Duration}");
 
-            var second = new Stopwatch();
-            second.Start();
-            Thread.Sleep(5000);
-            second.Stop();
-            Console.WriteLine($"Second stopwatch is {second.Duration}");
+             var second = new Stopwatch();
+             second.Start();
+             Thread.Sleep(5000);
+             second.Stop();
+             Console.WriteLine($"Second stopwatch is {second.Duration}");*/
+
+            var post = new Post();
+            post.Title = "Test";
+            Console.WriteLine($"Post Title- {post.Title} was created {post.Created}");
+
+            post.UpVote();
+            post.UpVote();
+            post.UpVote();
+            Console.WriteLine($"Post Count = {post.VoteCount}");
+            post.DownVote();
+            Console.WriteLine($"Post Count = {post.VoteCount}");
         }
     }
 }
---
+++ Exercises/Classes/Post.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises.Classes
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; } = DateTime.Now;
+
+        public int VoteCount { get; private set; }
+
+        public int UpVote()
+        {
+            return VoteCount++;
+        }
+
+        public int DownVote()
+        {
+            return VoteCount--;
+        }
+    }
+}
